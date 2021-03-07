4ae03b5bf0dc79b56d5a6e62bfdae2480de6fd89
Meghan Fairbanks <meghan@fairbanks.ink>
2021-03-07 15:18:40 -0600

    export vDatastore

--- rvtools.ps1
+++ rvtools.ps1
@@ -1,7 +1,9 @@
 . ".\variables.ps1"
 
-Write-Output $RVToolsPath
 set-location $RVToolsPath
 
 $arguments = "-u $VMWuser -p $VMWpass -s $VMWserv -c ExportvHealth2xlsx -d $outpath -f vHealth.xlsx"
+Start-Process -FilePath ".\RVTools.exe" -ArgumentList $arguments
+
+$arguments = "-u $VMWuser -p $VMWpass -s $VMWserv -c ExportvDatastore2xlsx -d $outpath -f vDatastore.xlsx"
 Start-Process -FilePath ".\RVTools.exe" -ArgumentList $arguments
