88434225932041cf65f49850fdbdafe6faa41893
Meghan Fairbanks <meghan@fairbanks.ink>
2021-03-07 15:11:32 -0600

    export vHealth.ps1

--- rvtools.ps1
+++ rvtools.ps1
@@ -3,5 +3,5 @@
 Write-Output $RVToolsPath
 set-location $RVToolsPath
 
-$arguments = "-u $VMWuser -p $VMWpass -s $VMWserv"
+$arguments = "-u $VMWuser -p $VMWpass -s $VMWserv -c ExportvHealth2xlsx -d $outpath -f vHealth.xlsx"
 Start-Process -FilePath ".\RVTools.exe" -ArgumentList $arguments
