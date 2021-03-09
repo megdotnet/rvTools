f891b947521129e8bc9b2c6b49e86f335ee1ea12
Meghan Fairbanks <meghan@fairbanks.ink>
2021-03-08 20:24:57 -0600

    Add email ability to email reports

--- rvtools.ps1
+++ rvtools.ps1
@@ -13,3 +13,7 @@ Start-Process -FilePath ".\RVTools.exe" -ArgumentList $arguments
 
 $arguments = "-u $VMWuser -p $VMWpass -s $VMWserv -c ExportvDatastore2xlsx -d $outpath -f vDatastore.xlsx"
 Start-Process -FilePath ".\RVTools.exe" -ArgumentList $arguments
+
+Start-Sleep -Seconds 15
+
+Send-MailMessage -From $from -To $to -Subject $subject -Body $body -SmtpServer $smtpserver -Credential $credential -Usessl -Port 587 -Attachments $attach1,$attach2
